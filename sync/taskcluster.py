62385c39ff4074c4ba406173dd9baaef59dbbb12
Maja Frydrychowicz <mjzffr@gmail.com>
2017-09-06 16:56:48 -0400

    Add helpers for interacting with TaskCluster API

---
+++ sync/taskcluster.py
@@ -0,0 +1,92 @@
+import os
+import requests
+import shutil
+import traceback
+
+import log
+
+QUEUE_BASE = "https://queue.taskcluster.net/v1/"
+ARTIFACTS_BASE = "https://public-artifacts.taskcluster.net/"
+
+logger = log.get_logger("upstream")
+
+
+def is_suite(task, suite):
+    t = task.get("task", {})
+    metadata = t.get("metadata")
+    if metadata and metadata.get("name"):
+        return suite in metadata["name"]
+
+
+def is_build(task):
+    tags = task.get("tags")
+    if tags:
+        return tags.get("kind") == "build"
+
+
+def is_completed(task):
+    status = task.get("status")
+    if status:
+        return status.get("state") == "completed"
+
+
+def filter_suite(tasks, suite):
+    # expects return value from get_tasks_in_group
+    return [t for t in tasks if is_suite(t, suite)]
+
+
+def filter_completed(tasks):
+    # expects return value from get_tasks_in_group
+    # completed implies that it did not fail
+    return [t for t in tasks if is_completed(t)]
+
+
+def get_tasks_in_group(group_id):
+    list_url = QUEUE_BASE + "task-group/" + group_id + "/list"
+
+    r = requests.get(list_url, params={
+        "limit": 200
+    })
+    reply = r.json()
+    tasks = reply["tasks"]
+    while "continuationToken" in reply:
+        r = requests.get(list_url, params={
+            "limit": 200,
+            "continuationToken": reply["continuationToken"]
+        })
+        reply = r.json()
+        tasks += reply["tasks"]
+    return tasks
+
+
+def download_logs(tasks, destination, retry=5):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    url = ARTIFACTS_BASE + "{task}/{run}/public/logs/live_backing.log"
+    log_files = []
+    for task in tasks:
+        status = task.get("status", {})
+        for run in status.get("runs", []):
+            params = {
+                "task": status["taskId"],
+                "run": run["runId"],
+            }
+            log_url = url.format(**params)
+            log_name = "live_backing-{task}_{run}.log".format(**params)
+            success = False
+            logger.debug("Trying to download {}".format(log_url))
+            while not success and retry > 0:
+                try:
+                    r = requests.get(log_url, stream=True)
+                    log_path = os.path.abspath(os.path.join(destination, log_name))
+                    with open(log_path, 'wb') as f:
+                        r.raw.decode_content = True
+                        shutil.copyfileobj(r.raw, f)
+                    success = True
+                    log_files.append(log_path)
+                except Exception as e:
+                    logger.warning(traceback.format_exc(e))
+                    retry -= 1
+            if not success:
+                logger.warning("Failed to download log from {}".format(log_url))
+    return log_files
