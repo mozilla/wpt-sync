d6d592566c746f501e66526ff817ed4603b8d386
Maja Frydrychowicz <mjzffr@gmail.com>
2018-01-19 19:34:21 -0500

    Add helper script for running docker in dev env

---
+++ docker/run-docker-wpt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $1 == "build" ]]; then
+    docker build -t wptsync_dev --file docker/dev/Dockerfile .
+elif [[ $1 == "test" ]]; then
+    exec docker run -it wptsync_dev --test
+else
+    exec docker run --init -it --add-host=rabbitmq:127.0.0.1 \
+    --env WPTSYNC_CONFIG=/app/wpt-sync/devenv/sync_maja.ini \
+    --env WPTSYNC_CREDS=/app/wpt-sync/devenv/credentials_maja.ini \
+    --env WPTSYNC_SSH_CONFIG=/app/wpt-sync/devenv/ssh_config \
+    --env WPTSYNC_GECKO_CONFIG=/app/wpt-sync/devenv/gecko_config \
+    --env WPTSYNC_WPT_CONFIG=/app/wpt-sync/devenv/wpt_config \
+    --mount type=bind,source=$(pwd),target=/app/wpt-sync \
+    --mount type=bind,source=$(pwd)/repos,target=/app/repos \
+    --mount type=bind,source=$(pwd)/workspace,target=/app/workspace wptsync_dev $@
+fi
+
