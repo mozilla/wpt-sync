abb7e1faaac388f223c6ea78f5a72ee522127523
Maja Frydrychowicz <mjzffr@gmail.com>
2018-02-05 12:11:40 -0500

    Configure docker steps for ansible playbook

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+# Run this from wptsync repo root
 
-ANSIBLE_CONFIG="ansible/ansible.cfg" ansible-playbook -i ansible/hosts -f 20 ansible/wptsync_deploy.yml -vvv
+ANSIBLE_CONFIG="ansible/ansible.cfg" ansible-playbook -i ansible/hosts -f 20 \
+    ansible/wptsync_deploy.yml -vvv \
+    --extra-vars repo_root=$(pwd) \
+    --extra-vars image_name=wptsync_dev \
+    --extra-vars tempdir=$(pwd)/devenv/ansible_workspace
