91582ea97df8990ad7b9b02c1a2709c56aea8ef2
Maja Frydrychowicz <mjzffr@gmail.com>
2018-05-28 11:54:09 -0400

    Add mandatory _image_name variable to update script.
    The Jijna template for run_docker.sh relies on _image_name.
    This was always a mandatory variable but ansible <2.5
    ansible was not properly enforcing that so it didn't come up
    as an error.

--- bin/update_server.sh
+++ bin/update_server.sh
@@ -2,5 +2,13 @@
 # Run this from wptsync repo root
 set -euo pipefail
 
+# _image_name: name of docker image, tagged with relevant sha
+#   e.g. wptsync_dev:cdc0d642c5ba17914e001e2a9b9fb6c893cfe57c
+#   Since we're not rebuilding the image, provide name of most recent deployed image
+
+# This should work if we've been conscientious about tagging builds with *-image
+img="wptsync_dev:$(git rev-list -n 1 $(git tag --list .*-image | tail -1))"
+
 ANSIBLE_CONFIG="ansible/ansible.cfg" ansible-playbook -i ansible/hosts -f 20 \
-    ansible/wptsync_update.yml -vvv 
+    ansible/wptsync_update.yml -vvv \
+    --extra-vars _image_name=$img
