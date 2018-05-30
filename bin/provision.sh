b706b40ff520bd02b489076c18d7f3cd4396b0bc
Maja Frydrychowicz <mjzffr@gmail.com>
2018-05-30 15:21:58 -0400

    Update credentials only when required environment variables are set

--- bin/provision.sh
+++ bin/provision.sh
@@ -2,9 +2,11 @@
 # Run this from wptsync repo root
 set -euo pipefail
 
-# WPT_SSH_HGMO: path to private key; assuming pk is $WPT_SSH_HGMO.pub
-# WPT_SSH_GITHUB: path to private key; assuming pk is $WPT_SSH_GITHUB.pub
-# WPT_CREDENTIALS: path to production credentials ini
+# The "install credentials" step will be skipped unless all three of these
+# environment variables are set.
+#   WPT_SSH_HGMO: path to private key; assuming pk is $WPT_SSH_HGMO.pub
+#   WPT_SSH_GITHUB: path to private key; assuming pk is $WPT_SSH_GITHUB.pub
+#   WPT_CREDENTIALS: path to production credentials ini
 
 # _repo_root: path to wpt-sync repo
 # _image_name: name of docker image, tagged with HEAD sha
@@ -26,9 +28,6 @@ ANSIBLE_CONFIG="ansible/ansible.cfg" ansible-playbook -i ansible/hosts -f 20 \
     --extra-vars _repo_root=$(pwd) \
     --extra-vars _image_name=$img \
     --extra-vars _tempdir=$(pwd)/devenv/ansible_workspace \
-    --extra-vars _ssh_hgmo=$WPT_SSH_HGMO \
-    --extra-vars _ssh_github=$WPT_SSH_GITHUB \
-    --extra-vars _credentials=$WPT_CREDENTIALS
 
 echo Creating tag $tag. Remember to push it.
 git tag -a $tag -m "$msg"
