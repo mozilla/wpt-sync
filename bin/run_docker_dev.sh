701a8bad666adce28417053c6b0f13b291e3c50e
James Graham <james@hoppipolla.co.uk>
2019-04-25 22:25:05 +0100

    Fixup running tests

--- bin/run_docker_dev.sh
+++ bin/run_docker_dev.sh
@@ -29,7 +29,10 @@ if [[ $command == "build" ]]; then
     }
     docker build -t wptsync_dev --file docker/Dockerfile.dev .
 elif [[ $command == "test" ]]; then
-    exec docker run -it --mount type=bind,source=$(pwd),target=/app/wpt-sync wptsync_dev --test
+    exec docker run -it \
+         --env WPTSYNC_CONFIG=/app/wpt-sync/devenv/sync.ini \
+         --mount type=bind,source=$(pwd),target=/app/wpt-sync \
+         wptsync_dev --test $@
 elif [[ $command == "run" ]]; then
     exec docker run -it --add-host=rabbitmq:127.0.0.1 \
     --env WPTSYNC_CONFIG=/app/wpt-sync/devenv/sync.ini \
