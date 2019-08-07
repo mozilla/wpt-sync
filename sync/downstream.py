3e2645dbb093e163c3adf0f7e7078f2fb45690a7
David Heiberg <dheiberg@mozilla.com>
2019-08-07 16:47:22 +0100

    Override error setter to also add GitHub label

--- sync/downstream.py
+++ sync/downstream.py
@@ -121,6 +121,12 @@ class DownstreamSync(SyncProcess):
         merged unless it happens to be a fast-forward"""
         return sync_commit.WptCommit(self.git_wpt, "origin/pr/%s" % self.pr)
 
+    @SyncProcess.error.setter
+    def error(self, value):
+        if value is not None and self.pr:
+            env.gh_wpt.add_labels(self.pr, "mozilla:gecko-blocked")
+        return SyncProcess.error.fset(self, value)
+
     @property
     def pr_status(self):
         return self.data.get("pr-status", "open")
