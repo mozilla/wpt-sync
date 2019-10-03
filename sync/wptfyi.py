846b6bac5067428cefdfe9bc47376b1255b679f6
James Graham <james@hoppipolla.co.uk>
2019-10-03 18:06:27 +0100

    Add basic API for getting results off wpt.fyi

---
+++ sync/wptfyi.py
@@ -0,0 +1,99 @@
+import requests
+import time
+import urllib
+import urlparse
+
+
+WPT_FYI_BASE = "https://wpt.fyi/api/"
+
+
+class Url(object):
+    def __init__(self, initial_url):
+        if initial_url:
+            parts = urlparse.urlsplit(initial_url)
+            self.scheme = parts.scheme
+            self.host = parts.netloc
+            self.path = parts.path
+            self.query = urlparse.parse_qsl(parts.query, keep_blank_values=True)
+            self.fragment = parts.fragment
+        else:
+            self.scheme = ""
+            self.host = ""
+            self.path = ""
+            self.query = []
+            self.fragment = ""
+
+    def scheme(self, value):
+        self.scheme = value
+        return self
+
+    def host(self, value):
+        self.host = value
+        return self
+
+    def path(self, value):
+        self.path = value
+        return self
+
+    def add_path(self, value):
+        self.path = urlparse.urljoin(self.path, value)
+        return self
+
+    def query(self, value):
+        self.query = value
+        return self
+
+    def add_query(self, name, value):
+        self.query.append((name, value))
+        return self
+
+    def fragment(self, value):
+        self.fragment = value
+        return self
+
+    def build(self):
+        return urlparse.urlunsplit((self.scheme,
+                                    self.host,
+                                    self.path,
+                                    urllib.urlencode(self.query),
+                                    self.fragment))
+
+
+def get_runs(sha=None, pr=None, max_count=None, labels=None):
+    url = Url(WPT_FYI_BASE + "runs")
+
+    for name, value in [("sha", sha), ("pr", pr), ("max-count", max_count)]:
+        if value is not None:
+            url.add_query(name, value)
+    if labels:
+        for item in labels:
+            url.add_query("label", item)
+
+    resp = requests.get(url.build())
+    resp.raise_for_status()
+    return resp.json()
+
+
+def get_results(run_ids, test=None, query=None):
+    url = Url(WPT_FYI_BASE + "search")
+
+    body = {
+        "run_ids": run_ids
+    }
+    if query is not None:
+        body["q"] = query
+
+    # A 422 status means that the data isn't in the cache, so retry
+    retry = 0
+    timeout = 10
+
+    while retry < 5:
+        resp = requests.post(url.build(), json=body)
+        if resp.status_code != 422:
+            break
+        time.sleep(timeout)
+        retry += 1
+        timeout *= 1.5
+
+    resp.raise_for_status()
+    return resp.json()
