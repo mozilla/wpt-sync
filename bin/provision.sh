8ef52c86701f76a078d3aee39b992bb1087ad49f
James Graham <james@hoppipolla.co.uk>
2020-08-21 14:51:10 +0100

    Fix setting Python settings during deployment

--- bin/provision.sh
+++ bin/provision.sh
@@ -40,6 +40,8 @@ ANSIBLE_CONFIG="ansible/ansible.cfg" ansible-playbook -i ansible/hosts -f 20 \
     --extra-vars _repo_root=$(pwd) \
     --extra-vars _image_name=$img \
     --extra-vars _tempdir=$(pwd)/ansible_workspace \
+    --extra-vars _python_binary=python \
+    --extra-vars _pip_binary=pip \
 
 echo Creating tag $tag. Remember to push it.
 git tag -a $tag -m "$msg"
